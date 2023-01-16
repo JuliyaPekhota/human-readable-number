336d7239f1167513ffff2305939111b2cbc2c954
Juliya Pekhota <konyliyaevg@gmail.com>
2023-01-16 15:04:33 +0300

    init: task Human Readable Number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function toReadable (number) {
-  
+  const a = ['zero','one','two','three','four', 'five','six','seven','eight','nine', 'ten', 'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  const b = ['zero','ten', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+  let result = "";
+  let fillArray = [...Array(3).fill("0"), ...number.toString()];
+
+if (number === 0) result = "zero";
+
+fillArray = fillArray.slice(-3);
+
+if (Number(fillArray[0]) !== 0) {
+  result += `${a[Number(fillArray[0])]} hundred `;
 }
+if (Number(fillArray[1]) !== 0 && Number(fillArray[1]) > 1) {
+  result += `${b[Number(fillArray[1])]} `;
+} 
+if (Number(fillArray[1]) !== 0 && Number(fillArray[1]) === 1) {
+  result += `${a[Number(`${fillArray[1]}${fillArray[2]}`)]} `;
+}
+if (Number(fillArray[2]) !== 0 && (Number(fillArray[1]) > 1 || Number(fillArray[1]) === 0)) {
+  result += `${a[Number(fillArray[2])]}`;
+}
+      
+     return result.trim();
+}
+
